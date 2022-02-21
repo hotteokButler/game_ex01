3aa9c1cbd5a7b3b80c9e17092c8d9834945883e6
jisooyoon <ygs12567@gmail.com>
2022-02-21 18:22:50 +0900

    Add(app.js) : 게임 전반적 인터페이스
    - 벌레 선택시 'you lost'라는 창과 함께 게임 정지
    - 당근 전부 선택시 'you win'이라는 창과 함께 게임정지
    - 당근 카운팅,
    - 리셋시 시간도 리셋
    - 개선필요사항 : 게임 리셋시 빠르게 줄어드는것 , 게임 플레이시 일시정지 버튼으로 모양 바꾸기,

--- js/app.js
+++ js/app.js
@@ -6,12 +6,31 @@ const bug = document.querySelector('#bug');
 const targetBox = document.querySelector('.targets');
 const resetBtn = document.querySelector('#reset-btn');
 const resetBox = document.querySelector('.game-lost');
+const gameLost = document.querySelector('.game-lost__txt');
+const gameWin = document.querySelector('.game-win__txt');
+
+const GAME_RESULT_CLASS = 'game-result-display';
 
 let gameStart = false;
 let carrotList = [];
 let bugList = [];
 let time;
 let count;
+
+const audioBk = new Audio('sound/bg.mp3');
+audioBk.volume = 0.7;
+
+const audioAlert = new Audio('sound/alert.mp3');
+audioAlert.volume = 0.7;
+
+const audioWin = new Audio('sound/game_win.mp3');
+audioAlert.volume = 0.7;
+
+const audioCarrot = new Audio('sound/carrot_pull.mp3');
+audioAlert.volume = 0.4;
+
+const audioBug = new Audio('sound/bug_pull.mp3');
+audioAlert.volume = 0.4;
 
 function randomOffset(length) {
   let randomValue = Math.floor(Math.random() * length);
@@ -55,43 +74,78 @@ function randomTargets(num) {
 function start() {
   timmer.textContent = `00:${String(time).padStart(2, '0')}`;
   time--;
-
   if (time < 0) {
     clearInterval(start);
+    audioBk.pause();
     resetBox.classList.remove('hidden');
     resetBox.classList.add('show');
+    audioAlert.play();
     time = 0;
   }
 }
 
 function removeTarget(event) {
   let killTarget = event.target;
+
   if (killTarget.id === 'bug') {
     gameStart = true;
+    gameLost.classList.remove(GAME_RESULT_CLASS);
+    gameWin.classList.add(GAME_RESULT_CLASS);
     resetBox.classList.add('show');
     resetBox.classList.remove('hidden');
+    clearInterval(start);
+    time = 0;
+    audioBug.play();
+    audioBk.pause();
+    audioAlert.play();
   } else if (killTarget.id === 'carrot') {
     targetBox.removeChild(killTarget);
+    counter.textContent = `${count}`;
     count--;
+    audioCarrot.load();
+    audioCarrot.play();
+
+    if (count < 0) {
+      gameStart = true;
+      audioBk.pause();
+      audioWin.play();
+
+      gameWin.classList.remove(GAME_RESULT_CLASS);
+      gameLost.classList.add(GAME_RESULT_CLASS);
+      resetBox.classList.add('show');
+      resetBox.classList.remove('hidden');
+      clearInterval(start);
+      time = 0;
+      targetBox.removeEventListener(removeTarget);
+    }
   }
 }
 
 function onPlayGame(event) {
   if (gameStart === false) {
     gameStart = true;
+
+    audioBk.play();
+
     targetBox.classList.remove('hidden');
     targetBox.classList.add('show');
 
     randomTargets(7);
     spreadTarget();
+    clearInterval(start);
 
     time = 15;
     setInterval(start, 1000);
+    counter.textContent = `${count + 1}`;
 
     targetBox.addEventListener('click', removeTarget);
-  } else if (gameStart === ture) {
+  } else if (gameStart === true) {
+    audioBk.pause();
+    rePlayGame();
     playBtn.removeEventListener(onPlayGame);
     targetBox.removeEventListener(removeTarget);
+    clearInterval(start);
+    time = 0;
   }
 }
 
@@ -103,6 +157,7 @@ function rePlayGame() {
   resetBox.classList.remove('show');
   resetBox.classList.add('hidden');
   onPlayGame();
+  window.location.reload();
 }
 
 playBtn.addEventListener('click', onPlayGame);
