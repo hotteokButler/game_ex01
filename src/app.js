f3462795a3426cb27d576bc22edf80b40f567d20
jisooyoon <ygs12567@gmail.com>
2022-03-10 17:11:09 +0900

    Refactor : Builder Pattern 적용
    - game.js 모듈 생성 및 Builder Pattern 적용

--- src/app.js
+++ src/app.js
@@ -1,142 +1,38 @@
 'use strict';
 
 import PopUp from './popup.js';
-import Field from './field.js';
-import * as sound from './sound.js';
+import Game from './game.js';
+import GameBuilder from './game.js';
 
 (function () {
-  const CARROT_COUNT = 15;
-  const BUG_COUNT = 15;
-  const GAME_DURATION_SEC = 18;
-
-  const gameBtn = document.querySelector('.game__button');
-  const gameTimer = document.querySelector('.game__timer');
-  const gameScore = document.querySelector('.game__score');
-
-  let started = false;
-  let score = 0;
-  let timer = undefined;
-
   const gameFinishBanner = new PopUp();
-  const gameField = new Field(CARROT_COUNT, BUG_COUNT);
+  const game = new GameBuilder()
+    .gameDuration(18) //
+    .carrotCount(15) //
+    .bugCount(15) //
+    .build();
+
+  game.setGameStopListner((reason) => {
+    let message;
+
+    switch (reason) {
+      case 'win':
+        message = 'You Won!👏👏👏';
+        break;
+      case 'lose':
+        message = 'You Lost!💩💩💩';
+        break;
+      case 'cancel':
+        message = 'REPLAY?😲';
+        break;
+      default:
+        throw new Error('not valid reason');
+    }
+    gameFinishBanner.showWithText(message);
+  });
 
   gameFinishBanner.setClickListener(() => {
-    startGame();
-    showGameButton();
+    game.start();
+    game.showGameButton();
   });
-
-  gameField.setClickListner(onItemClick);
-
-  function onItemClick(item) {
-    if (!started) {
-      return;
-    }
-    if (item === 'carrot') {
-      score++;
-
-      updateScoreBoard();
-      if (score === CARROT_COUNT) {
-        finishGame(true);
-      }
-    } else if (item === 'bug') {
-      stopGameTimer();
-      finishGame(false);
-    }
-  }
-
-  gameBtn.addEventListener('click', () => {
-    if (started) {
-      stopGame();
-    } else {
-      startGame();
-    }
-  });
-
-  function startGame() {
-    started = true;
-    initGame();
-    showStopButton();
-    showTimerAndScore();
-    startGameTimer();
-    sound.playBackground();
-  }
-  function stopGame() {
-    started = false;
-    stopGameTimer();
-    hideGameButton();
-    gameFinishBanner.showWithText('REPLAY????');
-    hideTimerAndScore();
-    sound.stopBackground();
-    sound.playAlert();
-  }
-  function finishGame(win) {
-    started = false;
-    score = 0;
-    hideGameButton();
-    hideTimerAndScore();
-    // finishGame에서 한번 더 stopGameTimer을 호출해줘야지 중복되어 성공 후 리플레이시 타임이 빠르게 줄어드는 오류를 제거
-    stopGameTimer(timer);
-    if (win) {
-      sound.playWin();
-    } else {
-      sound.playAlert();
-      sound.stopBackground();
-    }
-    gameFinishBanner.showWithText(win ? 'YOU WON!!!!😎' : 'YOU LOST!!💩');
-  }
-  function stopGameTimer() {
-    clearInterval(timer);
-  }
-  function showStopButton() {
-    const stopIcon = gameBtn.querySelector('.game__button-stop');
-    const palyIcon = gameBtn.querySelector('.game__button-play');
-    stopIcon.classList.remove('game__button--hide');
-    palyIcon.classList.add('game__button--hide');
-  }
-  function showGameButton() {
-    gameBtn.style.visibility = 'visible';
-  }
-  function hideGameButton() {
-    gameBtn.style.visibility = 'hidden';
-  }
-  function showTimerAndScore() {
-    gameTimer.style.visibility = 'visible';
-    gameScore.style.visibility = 'visible';
-  }
-  function hideTimerAndScore() {
-    gameTimer.style.visibility = 'hidden';
-    gameScore.style.visibility = 'hidden';
-  }
-
-  function startGameTimer() {
-    let remaingTimeSec = GAME_DURATION_SEC;
-    updateTimerText(remaingTimeSec);
-    timer = setInterval(() => {
-      if (remaingTimeSec <= 0) {
-        clearInterval(timer);
-        started = false;
-        //finishGame의 argument가 boolean값으로 전달되고있으므로 조건식을 전달하여 중단유무
-        finishGame(CARROT_COUNT === score);
-        return;
-      }
-      updateTimerText(--remaingTimeSec);
-    }, 1000);
-  }
-  // 왜 분단위로 계산 후에 초단위로 다시 나눠주는걸까?
-  // -> 카운팅에 분단위도 들어있어서
-  function updateTimerText(time) {
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-    gameTimer.innerText = `${minutes} :${seconds}`;
-  }
-
-  function initGame() {
-    score = 0;
-    gameScore.innerText = CARROT_COUNT;
-    gameField.init();
-  }
-
-  function updateScoreBoard() {
-    gameScore.innerText = CARROT_COUNT - score;
-  }
 })();
